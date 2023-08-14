f4b7b186d6c81b9789a0565e913293ef1eb1d43a
Richard Sinclair <richardksinclair20@gmail.com>
2023-08-14 14:02:06 -0500

    Created ToDo model

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'to_do_fields.apps.ToDoFieldsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

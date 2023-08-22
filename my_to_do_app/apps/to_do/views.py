8ff573046c97a56bbc43642aa7d3125eb152f436
Richard Sinclair <richardksinclair20@gmail.com>
2023-08-22 14:39:33 -0500

    Added CSRF protection and made sure /add_todo url worked.

--- my_to_do_app/apps/to_do/views.py
+++ my_to_do_app/apps/to_do/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def index(request):
 
+    return render(request, 'to_do/index.html')
+
+
+def add_todo(request):
+    print(request.POST)
+    print("Added to-do;Redirecting to home")
     return render(request, 'to_do/index.html')

c189d2f03da65d52d2bb11db6099ec790d03d605
Richard Sinclair <richardksinclair20@gmail.com>
2023-08-23 14:57:59 -0500

    Made sure data grabbed from form to create db entries

--- my_to_do_app/apps/to_do/views.py
+++ my_to_do_app/apps/to_do/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.utils import timezone
+from apps.to_do.models import Todo
 
 # Create your views here.
 def index(request):
 
     return render(request, 'to_do/index.html')
 
-
+@csrf_protect
 def add_todo(request):
-    print(request.POST)
-    print("Added to-do;Redirecting to home")
+    done_by = timezone.now()
+    content = request.POST['content']
+    new_todo = Todo.objects.create(added_date = done_by, text = content)
     return render(request, 'to_do/index.html')

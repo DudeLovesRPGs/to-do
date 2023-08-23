a3cbbee8ef2756eda9a0b25d4b0c06118b32c18f
Richard Sinclair <richardksinclair20@gmail.com>
2023-08-23 15:21:01 -0500

    Todos update on page;added redirect to index

--- my_to_do_app/apps/to_do/views.py
+++ my_to_do_app/apps/to_do/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.utils import timezone
 from apps.to_do.models import Todo
 
 # Create your views here.
 def index(request):
-
-    return render(request, 'to_do/index.html')
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, 'to_do/index.html', {
+        "todos": todo_items
+    })
 
 @csrf_protect
 def add_todo(request):
     done_by = timezone.now()
     content = request.POST['content']
     new_todo = Todo.objects.create(added_date = done_by, text = content)
-    return render(request, 'to_do/index.html')
+    return HttpResponseRedirect("/todo/")
